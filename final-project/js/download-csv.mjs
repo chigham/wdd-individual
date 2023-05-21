3288e76adae8dd1376896a028cebc1662bfac4f1
chigham <chigham@byui.edu>
2023-05-20 22:42:14 -0600

    Download csv upon click.

--- final-project/js/download-csv.mjs
+++ final-project/js/download-csv.mjs
@@ -1,4 +1,22 @@
 export default async function downloadCSV(apiUrl) {
     apiUrl = apiUrl.replace("=JSON", "=CSV");
-    let junk = await fetch(apiUrl);
+    // Fetch the CSV file
+    const response = await fetch(apiUrl);
+
+    // Get the blob data from the response
+    const blob = await response.blob();
+
+    // Create a temporary URL for the blob
+    const url = URL.createObjectURL(blob);
+
+    // Create a link element and set its attributes
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "data.csv"; // Set the desired filename for the downloaded file
+
+    // Simulate a click event to trigger the file download
+    link.click();
+
+    // Clean up the temporary URL
+    URL.revokeObjectURL(url);
 }
