9a37b9c56b5a63741f9170883e3a0be9fbedd681
chigham <chigham@byui.edu>
2023-05-21 11:22:28 -0600

    One more try at js import statements.

--- final-project/js/button-execute.js
+++ final-project/js/button-execute.js
@@ -1,6 +1,6 @@
-import {apiResults, getDateResults, apiRequest} from "/final-project/js/power-api-results.mjs";
-import convertMMtoIN from "/final-project/js/convert-mm-to-in.mjs";
-import downloadCSV from "/final-project/js/download-csv.mjs";
+import {apiResults, getDateResults, apiRequest} from "/wdd-individual/final-project/js/power-api-results.mjs";
+import convertMMtoIN from "/wdd-individual/final-project/js/convert-mm-to-in.mjs";
+import downloadCSV from "/wdd-individual/final-project/js/download-csv.mjs";
 
 const form = document.getElementById('formElem');
 form.addEventListener('submit', createSubmitData);
