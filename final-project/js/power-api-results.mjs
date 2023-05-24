ae834777bff6465be46a9bfcea1b8f2e9fc86756
chigham <chigham@byui.edu>
2023-05-24 01:07:05 -0600

    Removed hr. Inject all params including min/max.

--- final-project/js/power-api-results.mjs
+++ final-project/js/power-api-results.mjs
@@ -10,20 +10,13 @@ export function getDateResults(temporal) {
         dateResults.Year = document.getElementById('day-year').value;
         dateResults.Month = document.getElementById('day-month').value;
         dateResults.Day = document.getElementById('day-day').value;
-    } else if (temporal == 'Hour') {
-        dateResults.Year = document.getElementById('hour-year').value;
-        dateResults.Month = document.getElementById('hour-month').value;
-        dateResults.Day = document.getElementById('hour-day').value;
-        dateResults.Hour = document.getElementById('hour-hour').value;
     } else if (temporal == 'Custom') {
         dateResults.Year1 = document.getElementById('startYear').value;
         dateResults.Month1 = document.getElementById('startMonth').value;
         dateResults.Day1 = document.getElementById('startDay').value;
-        dateResults.Hour1 = 0;
         dateResults.Year2 = document.getElementById('endYear').value;
         dateResults.Month2 = document.getElementById('endMonth').value;
         dateResults.Day2 = document.getElementById('endDay').value;
-        dateResults.Hour2 = 23;
     }
 
     return dateResults;
@@ -39,19 +32,17 @@ export function apiResults(indicator, temporal, lon, lat, dateResults) {
         let month = dateResults.Month.toString().padStart(2, '0');
         let day = dateResults.Day.toString().padStart(2, '0');
         apiUrl = `https://power.larc.nasa.gov/api/temporal/daily/point?parameters=${indicator}&community=RE&longitude=${lon}&latitude=${lat}&start=${dateResults.Year}${month}${day}&end=${dateResults.Year}${month}${day}&time-standard=lst&format=JSON&user=byuiwddchigham`;
-    } else if (temporal == "Hour") {
-        let month = dateResults.Month.toString().padStart(2, '0');
-        let day = dateResults.Day.toString().padStart(2, '0');
-        apiUrl = `https://power.larc.nasa.gov/api/temporal/hourly/point?parameters=${indicator}&community=RE&longitude=${lon}&latitude=${lat}&start=${dateResults.Year}${month}${day}&end=${dateResults.Year}${month}${day}&time-standard=lst&format=JSON&user=byuiwddchigham`;
     } else if (temporal == "Custom") {
         let month1 = dateResults.Month1.toString().padStart(2, '0');
         let day1 = dateResults.Day1.toString().padStart(2, '0');
         let month2 = dateResults.Month2.toString().padStart(2, '0');
         let day2 = dateResults.Day2.toString().padStart(2, '0');
-        apiUrl = `https://power.larc.nasa.gov/api/temporal/hourly/point?parameters=${indicator}&community=RE&longitude=${lon}&latitude=${lat}&start=${dateResults.Year1}${month1}${day1}&end=${dateResults.Year2}${month2}${day2}&time-standard=lst&format=JSON&user=byuiwddchigham`;
+        apiUrl = `https://power.larc.nasa.gov/api/temporal/daily/point?parameters=${indicator}&community=RE&longitude=${lon}&latitude=${lat}&start=${dateResults.Year1}${month1}${day1}&end=${dateResults.Year2}${month2}${day2}&time-standard=lst&format=JSON&user=byuiwddchigham`;
     }
     if (indicator == "PRECTOTCORR" && (temporal == "Year" || temporal == "Month")) {
         apiUrl = apiUrl.replace("PRECTOTCORR", "PRECTOTCORR_SUM");
+    } else if ((indicator == "WS2M" || indicator == "T2M") && (temporal == "Year" || temporal == "Month" || temporal == "Day")) {
+        apiUrl = apiUrl.replace(indicator, `${indicator},${indicator}_MIN,${indicator}_MAX`);
     }
     
     return apiUrl;
